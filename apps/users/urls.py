8f2dc1d9d9544ded10f3b157b4581bc524e5ae19
Allan Dominguez <54010125+allandominguez@users.noreply.github.com>
2026-03-03 15:29:00 +1100

    Add format suffixes to urls (#8)

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from .api import views
 
@@ -6,3 +7,5 @@ urlpatterns = [
     path("users/", views.UserListView.as_view()),
     path("users/search/email/", views.UserSearchView.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
