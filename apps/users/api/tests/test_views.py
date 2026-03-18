93a4d330ad23af26817072f5a790223d7980a93d
Allan Dominguez <allan.c.dominguez@gmail.com>
2026-03-18 21:58:38 +1100

    Configure JWT and fix tests
    Will still need session authentication for development.
    
    Reconfigured tests to work with JWT authentication.

--- apps/users/api/tests/test_views.py
+++ apps/users/api/tests/test_views.py
@@ -1,63 +1,74 @@
 import pytest
 from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
 
 @pytest.fixture
-def admin_user(db):
-    return User.objects.create_superuser(
+def auth_client(db):
+    api_client = APIClient()
+    admin_user = User.objects.create_superuser(
         email="admin@example.com",
         password="secret",  # pragma: allowlist secret
         first_name="Admin",
         last_name="User",
     )
+    refresh = RefreshToken.for_user(admin_user)
+    api_client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
+    return api_client
 
 
 class TestUserListViewIntegration:
-    def test_admin_user(self, client, admin_user):
-        client.force_login(admin_user)
-        response = client.get("/users/")
+    def test_admin_user(self, auth_client):
+        response = auth_client.get("/users/")
         assert response.status_code == 200
 
-    def test_user_details(self, client, admin_user):
-        client.force_login(admin_user)
-        response = client.get("/users/")
-        assert response.status_code == 200
-        assert response.data[0]["first_name"] == "Admin"
-        assert response.data[0]["last_name"] == "User"
+    def test_user_details(self, db):
+        api_client = APIClient()
+        user = User.objects.create_superuser(
+            email="admin@example.com",
+            password="secret",  # pragma: allowlist secret
+            first_name="Jane",
+            last_name="Doe",
+        )
+        refresh = RefreshToken.for_user(user)
+        api_client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
+        response = api_client.get("/users/")
+        assert response.data[0]["first_name"] == "Jane"
+        assert response.data[0]["last_name"] == "Doe"
         assert response.data[0]["is_staff"] is True
 
 
 @pytest.mark.integration
 class TestUserSearchViewIntegration:
-    def test_user(self, client, admin_user):
+    def test_user(self, auth_client):
+        email = "test@example.com"
         User.objects.create_user(
-            email="test@example.com",
+            email=email,
             password="secret",  # pragma: allowlist secret
             first_name="Jane",
             last_name="Doe",
         )
-        client.force_login(admin_user)
-        response = client.get("/users/search/email/?email=test@example.com")
+        response = auth_client.get(f"/users/search/email/?email={email}")
         assert response.status_code == 200
         assert response.data["first_name"] == "Jane"
         assert response.data["is_staff"] is False
 
-    def test_superuser(self, client, admin_user):
+    def test_superuser(self, auth_client):
+        email = "test@example.com"
         User.objects.create_superuser(
-            email="test@example.com",
+            email=email,
             password="secret",  # pragma: allowlist secret
             first_name="Jane",
             last_name="Doe",
         )
-        client.force_login(admin_user)
-        response = client.get("/users/search/email/?email=test@example.com")
+        response = auth_client.get(f"/users/search/email/?email={email}")
         assert response.status_code == 200
         assert response.data["first_name"] == "Jane"
         assert response.data["is_staff"] is True
 
-    def test_non_existing_user(self, client, admin_user):
-        client.force_login(admin_user)
-        response = client.get("/users/search/email/?email=test@example.com")
+    def test_non_existing_user(self, auth_client):
+        response = auth_client.get("/users/search/email/?email=test@example.com")
         assert response.status_code == 404
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import include, path
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .api import views
 
@@ -7,6 +11,8 @@ urlpatterns = [
     path("users/", views.UserListView.as_view()),
     path("users/search/email/", views.UserSearchView.as_view()),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
